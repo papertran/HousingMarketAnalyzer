b29b5c6cfecdc12e5f13f6f46c1e4059a05f35d4
Kevin <kevintrn99@gmail.com>
2020-06-12 13:00:46 -0400

    setup for urls and views

--- data/views.py
+++ data/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index_view(request):
+	# Use context to pass in data to webpage
+	context = {}
+	return render(request, 'data/index.html', context=context)
