ecef2336c3e8fe2c5adcce572fdecda2748395ba
sean <yuhsredux@gmail.com>
2020-06-12 14:19:49 -0400

    Added Views
    started adding the needed views

--- data/urls.py
+++ data/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path 
 from . import views
 urlpatterns = [
-	path('', views.index_view, name='index')
+	path('', views.index_view, name='index'),
+	path('home/', views.index_view, name='home'),
+	path('future/', views.future_view, name='future'),
+	path('current/', views.current_view, name='current')
+
 ]
--- data/views.py
+++ data/views.py
@@ -5,3 +5,11 @@ def index_view(request):
 	# Use context to pass in data to webpage
 	context = {}
 	return render(request, 'data/index.html', context=context)
+
+def future_view(request):
+	context = {}
+	return render(request, 'data/future.html', context=context)
+
+def current_view(request):
+	context = {}
+	return render(request, 'data/current.html', context=context)
