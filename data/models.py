199c74f678bd0507540376fedc068edce8eb05bb
tranthanhphat203@gmail.com <tranthanhphat203@gmail.com>
2020-06-12 14:37:00 -0400

    create data model

--- data/models.py
+++ data/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class House(models.Model):
+    transDate = models.FloatField()
+    houseAge = models.FloatField()
+    distToNearStation = models.FloatField()
+    numStore = models.FloatField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    price = models.FloatField()
+	predicted = models.BooleanField()
+    
