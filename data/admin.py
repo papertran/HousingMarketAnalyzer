c2d668d63fdb8cdaa92737df558863dc23ff1532
sean <yuhsredux@gmail.com>
2020-06-19 14:46:46 -0400

    modified admin page

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import House, Price
+
+class HouseAdmin(admin.ModelAdmin):
+	list_display = ['houseId', 'region', 'HouseType']
+
+class PriceAdmin(admin.ModelAdmin):
+	list_display = ['houseId', 'Price']
+
+admin.site.register(House, HouseAdmin)
+admin.site.register(Price, PriceAdmin)
--- data/models.py
+++ data/models.py
@@ -4,11 +4,30 @@ from django.db import models
 class House(models.Model):
     houseId = models.IntegerField()
     region = models.CharField(max_length=100)
-    HouseType = models.CharField(max_length=100)
+    type_choices = [
+        ('Single', 'Single Family'),
+        ('Condo', 'Condo'),
+        ('Top', 'Top Tier'),
+        ('Middle', 'Middle Tier'),
+        ('Bottom', 'Bottom Tier'),
+        ('Studio', 'Studio'),
+        ('One', 'One Bed'),
+        ('Two', 'Two Bed'),
+        ('Three', 'Three Bed'),
+        ('Four', 'Four Bed'),
+        ('Many', 'Many Bed'),
+    ]
+    HouseType = models.CharField(max_length=100, choices=type_choices)
+
+    def __str__(self):
+        return "{} - {} - {}".format(self.houseId, self.region, self.HouseType)
 
 class Price(models.Model):
     priceId = models.IntegerField()
-    houseId = models.IntegerField()
-    listingDate = models.DateTimeField()
+    houseId = models.ForeignKey(House, on_delete=models.CASCADE)
+    listingDate = models.DateField(blank=False)
     Price = models.IntegerField()
+
+    def __str__(self):
+        return "{} - {} - {}".format(self.houseId, self.listingDate, self.Price)
     
