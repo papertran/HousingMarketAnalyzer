71cf7f09b2ed6357b1a72739e9b66506fd4ee79c
tranthanhphat203@gmail.com <tranthanhphat203@gmail.com>
2020-07-19 16:25:55 -0400

    Generate all data with predict price

--- DataAnalysis/LinReg.py
+++ DataAnalysis/LinReg.py
@@ -7,60 +7,67 @@ import math
 import sklearn
 from sklearn import linear_model, preprocessing, svm
 
-pd.set_option("display.max_rows", None, "display.max_columns", None)
-df = pd.read_excel("united-states (1).xls", sheet_name="All Homes")
+def Forecast(df, forecast_col, predictL):
+	df.fillna(value=-99999, inplace=True)
+	forecast_out = int(math.ceil(0.05 * len(df)))
 
-df = df.T
-df.drop(df.iloc[:, 1:], inplace = True, axis = 1)
-df.drop(df.index[0:3], inplace = True)
+	df['label'] = df[forecast_col].shift(-forecast_out)
 
-forecast_col = 0
-df.fillna(value=-99999, inplace=True)
-forecast_out = int(math.ceil(0.1 * len(df)))
+	x = np.array(df.drop(['label'], 1))
+	x = preprocessing.scale(x)
+	x_lately = x[-forecast_out:]
+	x = x[:-forecast_out]
 
-df['label'] = df[forecast_col].shift(-forecast_out)
+	df2 = df.drop(['label'], axis=1)
+	df.dropna(inplace=True)
 
-print("Before put in x\n", df)
-print()
+	y = np.array(df['label'])
 
-x = np.array(df.drop(['label'], 1))
-x = preprocessing.scale(x)
-x_lately = x[-forecast_out:]
-x = x[:-forecast_out]
+	x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-print("After put in x\n", df)
-print()
+	clf = linear_model.LinearRegression()
+	clf.fit(x_train, y_train)
+	accuracy = clf.score(x_test, y_test)
 
-df.dropna(inplace=True)
+	forecast_set = clf.predict(x_lately)
+	# print(forecast_set, accuracy, forecast_out)
+	predictL.append(forecast_set)
 
-y = np.array(df['label'])
+def getColumn(x, df):
+	df = df.drop(df.iloc[:, x+1:], axis = 1)
+	df = df.drop(df.iloc[:, :x], axis = 1)
+	return df
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
+if __name__ == "__main__":
+	predictList = []
 
-clf = linear_model.LinearRegression()
-clf.fit(x_train, y_train)
-accuracy = clf.score(x_test, y_test)
+	pd.set_option("display.max_rows", None, "display.max_columns", None)
+	df = pd.read_excel("united-states (1).xls", sheet_name="Two Bed")
+	df1 = df.T
+	df1.drop(df1.index[0:3], inplace = True)
 
-forecast_set = clf.predict(x_lately)
-print(forecast_set, accuracy, forecast_out)
-df['Forecast'] = np.nan
+	for i in range(0, len(df1.columns)):
+		temp = df1
+		temp = getColumn(i, temp)
+		Forecast(temp, i, predictList)
 
-last_date = df.iloc[-1].name
-last_unix = last_date.timestamp()
-one_month = 2628000
-next_unix = last_unix + one_month
-
-for i in forecast_set:
-    next_month = datetime.datetime.fromtimestamp(next_unix)
-    next_unix += 2628000
-    df.loc[next_month] = [np.nan for _ in range(len(df.columns)-1)]+[i]
-
-print(df)
-
-df[0].plot()
-df['label'].plot()
-df['Forecast'].plot()
-plt.legend(loc=4)
-plt.xlabel('Date')
-plt.ylabel('Price')
-plt.show()
+	last_date = df1.iloc[-1].name
+	last_unix = last_date.timestamp()
+	one_month = 2628000
+	next_unix = last_unix + one_month
+	predictTime = []
+	for i in range(6):
+		next_month = datetime.datetime.fromtimestamp(next_unix)
+		next_unix += one_month
+		predictTime.append(next_month)
+	
+	df2 = pd.DataFrame(predictList, columns=predictTime)
+	df2 = df2.T
+	result = df1.append(df2)
+	
+	df = df.T
+	df.drop(df.index[3:], inplace = True)
+	df = df.append(result)
+	df = df.T
+	df.set_index(df.columns[0], inplace = True)
+	df.to_csv("Predict Two Bed.csv")
